3591a96d610a988ad08952328b8f41a4acd4f136
Vanady-Beard <vanadybeard9@gmail.com>
2024-06-26 09:21:06 -0400

    initial commit

---
+++ mood_responses.py
@@ -0,0 +1,9 @@
+def mood_response(mood):
+   if mood == "happy":
+        return("I am happy that you feeling happy today. ")
+   if mood == "sad":
+        return("I hope you feel better ")
+   if mood == "excited":
+       return ("You are going to have a wonderful day !!!!")
+
+    
